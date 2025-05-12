737c4c870fefab5ce42be999071dddd7bbf3d9f1
Arthur moreira <arthurmsouza416@gmail.com>
2025-05-11 21:31:46 -0300

    lição completa

---
+++ main.js
@@ -0,0 +1,8 @@
+//bloco de código que desempenha um papel especifico que só será executado quando for chamado
+
+function somar() {
+    alert("Seja muito bem-vinda(a)!")
+    console.log("Mais uma vez, seja bem-vindo(a)")
+}
+
+mensagem()
